6ed7f80b1343684d84682f44f505a952e067173d
jjh <jjhroy@dingtalk.com>
2023-04-01 11:27:39 +0800

    commit

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>about</div>
+  <div class="flex">
+    <div
+      class="mx-auto mt-20 rounded-lg w-[387px] h-[180px] bg-[#fff] shadow-[0_2px_16px_rgba(16,85,189,0.06)] pl-6 pr-5 py-6"
+    >
+      <div class="text-[20px] leading-7 font-semibold text-[#111827]">
+        AI文章创意
+        <img src="https://img.amz123.com/static/images/start.svg" />
+      </div>
+      <div class="text-[##6B7280] text-[15px] leading-[26px] mt-[10px] mb-4">
+        该部分字段长度限定该部分字段长度限定该部分字段长度限定该部分字段长度限定该部分字段长度限…
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
-
-<style scoped></style>
